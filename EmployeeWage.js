f422fa334ec986f3be09944c3022eb22cb30441a
NayanTr09 <nayantripathi42@gmail.com>
2022-02-10 21:19:23 +0530

    Added : Ability to calculate Wages for 20 Days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -7,11 +7,11 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 let empHrs = 0;
-empCheck = Math.floor(Math.random()*10) %3;
+let empCheck = Math.floor(Math.random()*10) %3;
 function getWorkingHours(empCheck){
-
     switch(empCheck) {
         case IS_FULL_TIME:
             return FULL_TIME_HOURS;
@@ -21,7 +21,8 @@ function getWorkingHours(empCheck){
             return 0;
         }
     }
-    
-    empHrs = getWorkingHours(empCheck);
-    let empWage = empHrs*WAGE_PER_HOUR;
-    console.log("Emp Wage :" +empWage);
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++) {
+      empHrs+= getWorkingHours(empCheck);
+    }
+let empWage = empHrs*WAGE_PER_HOUR;
+    console.log("Total Hours :"+empHrs+ "Emp Wage :" +empWage);
